da431ca2d4c03069511ba0d581b0c95aa79e637d
gabrielhenriquedeseouzasouza <116382510+gabrielhenriquedeseouzasouza@users.noreply.github.com>
2022-10-21 16:11:10 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -7,7 +7,7 @@ import streamlit as st
 st.title("SENAI ZERBINI É NOTA 1000!")
 
 # Use st.header("") para adicionar um CABEÇALHO ao seu Web app
-st.header("BEM VINDOS AO ROGERIO!")
+st.header("MEU NOME EH YOSHIKAGE KIRA TENHO 33 ANOS!")
 
 # Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
 st.subheader("Bem vindos!")
